74d8490f03afc23de00e258a23f848f4bea8aa4a
Kevin Woulfe <kevinpwoulfe@gmail.com>
2018-06-15 12:31:14 -0700

    Update file structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,9 @@ import './Header.css';
 const Header = props => (
   <div className="header">
     <div className="title">{props.children}</div>
-    <div className="instructions">Click an image to begin!</div>
+    <div className="instructions">
+      Click an image to begin! But don't click the same image twice!
+    </div>
     <div className="scores">
       Score: {props.score} Highscore: {props.highscore}
     </div>
